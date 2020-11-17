9d4ff8c2f681f3911c20de130d28d14c6147e909
OlegBiletsky <ol.biletsky@gmail.com>
2020-11-17 22:40:25 +0200

    added logo Frontend Developer

--- src/components/SidebarLeft/Logo/Logo.js
+++ src/components/SidebarLeft/Logo/Logo.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Logo.css';
+import logo from './logo.png';
 
 function Logo() {
   return (
     <div className="Logo">
-      <h1 className='main-logo'>БО</h1>
+      {/* <h1 className='main-logo'>БО</h1> */}
+      <img src={logo} className='main-logo' />
     </div>
   );
 }
