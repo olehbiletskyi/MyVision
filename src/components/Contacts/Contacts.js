150034fde472daafae71b0045f3dcf057c2954e5
OlegBiletsky <ol.biletsky@gmail.com>
2020-11-26 12:04:51 +0200

    refac map 2

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -1,39 +1,33 @@
 import React from 'react';
-import './Contacts.css';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import './Contacts.css';
 
 function Contacts ({ BiggestCompanies, center }) {
-    // center: {
-    //     lat: 48.920827,
-    //     lng: 24.705657,
-    //     zoom: 15,
-    // },
-    //BiggestCompanies: [
-        // {
-        //     name: "ItCluster",
-        //     lat: 48.91942848361722,
-        //     lng: 24.709772442607548,
-        // },
 
     return (
         <MapContainer center={[center.lat,center.lng]} zoom={center.zoom} scrollWheelZoom={true}>
+
             <TileLayer
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
             />
 
-            <Marker position={[this.state.ItCluster.lat, this.state.ItCluster.lng]}>
-                <Popup className="popup">
-                    <div ><b>Адреса:</b> вулиця Січових стрільців, 17, Івано-Франківськ, Івано-Франківська область, 76000
-                    Години роботи: 
-                    Відчинено ⋅ Зачиняється: 18:00
-                    Телефон: 063 674 1219
-                    Зустрічі: itca.if.ua</div>
-                </Popup>
-            </Marker>
+            {BiggestCompanies.map((item)=>(
+                <Marker position={[item.lat, item.lng]} className="map-marker">
+                    <Popup className="map-marker-popup">
+                        <div className="map-marker-popup-body">
+                            <p className="">{ item.name}</p>
+                            <p className="">{item.description}</p>
+                            <p className="">{item.adress}</p>
+                            <p className="">{item.workingHours}</p>
+                            <p className="">{item.telephone}</p>
+                            <p className=""> <a href={`https://${item.website}`} target='blank'>{item.website}</a> </p> 
+                        </div>
+                    </Popup>
+                </Marker>
+            ))}        
             
         </MapContainer>
-      
     )
 }
 export default Contacts;
--- src/containers/ContactsContainer.js
+++ src/containers/ContactsContainer.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import Contacts from '../components/Contacts/Contacts';
 
 
-const ContactsContainer = ( { BiggestCompanies, center,  } ) => (
+const ContactsContainer = ( {BiggestCompanies, center,} ) => (
     <Contacts 
         BiggestCompanies={BiggestCompanies}
         center={center}
@@ -13,19 +13,10 @@ const ContactsContainer = ( { BiggestCompanies, center,  } ) => (
 
 
 
-
 const mapStateToProps = (state) => ({
     BiggestCompanies: state.mapReducer.BiggestCompanies,
     center: state.mapReducer.center,
 });
-
-
-// const mapDispatchToProps = (dispatch, param) => {
-//     return{
-//         getPosts: getPostsFunc(dispatch, param),
-//         changeInput: (inputValue) => dispatch( changeInputAction( inputValue ) ) 
-//     };
-// };
 
 
 export default connect(mapStateToProps)(ContactsContainer);
