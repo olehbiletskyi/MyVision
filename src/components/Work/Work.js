50ee669fe24e1fe07f9067fb6ed7c2327a5e44e5
OlegBiletsky <ol.biletsky@gmail.com>
2020-11-30 15:26:42 +0200

    added temporary works examples

--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -3,8 +3,95 @@ import './Work.css';
 
 function Work() {
   return (
-    <div className="Work">
-       <h1 className="test">Work</h1> 
+    <div className="Work-wrapper">
+        
+
+
+        <div className="Work-example e1">
+            <div className="Work-example-img-wrapper">
+                <img className="Work-example-img"   alt="" src=""/> 
+            </div>
+            <div className="Work-example__text-part">
+                <a href="#" className="Work-example__click" target='blank'>
+                    <h1 className="Work-example__title">Example 1</h1>
+                </a>
+            </div> 
+        </div>
+        <div className="Work-example e2">
+            <div className="Work-example-img-wrapper">
+                <img className="Work-example-img"   alt="" src=""/> 
+            </div>
+            <div className="Work-example__text-part">
+                <a href="#" className="Work-example__click" target='blank'>
+                    <h1 className="Work-example__title">Example 2</h1>
+                </a>
+            </div> 
+        </div>
+        <div className="Work-example e3">
+            <div className="Work-example-img-wrapper">
+                <img className="Work-example-img"   alt="" src=""/> 
+            </div>
+            <div className="Work-example__text-part">
+                <a href="#" className="Work-example__click" target='blank'>
+                    <h1 className="Work-example__title">Example 3</h1>
+                </a>
+            </div> 
+        </div>
+        <div className="Work-example e4">
+            <div className="Work-example-img-wrapper">
+                <img className="Work-example-img"   alt="" src=""/> 
+            </div>
+            <div className="Work-example__text-part">
+                <a href="#" className="Work-example__click" target='blank'>
+                    <h1 className="Work-example__title">Example 4</h1>
+                </a>
+            </div> 
+        </div>
+        <div className="Work-example e5">
+            <div className="Work-example-img-wrapper">
+                <img className="Work-example-img"   alt="" src=""/> 
+            </div>
+            <div className="Work-example__text-part">
+                <a href="#" className="Work-example__click" target='blank'>
+                    <h1 className="Work-example__title">Example 5</h1>
+                </a>
+            </div> 
+        </div>
+        <div className="Work-example e6">
+            <div className="Work-example-img-wrapper">
+                <img className="Work-example-img"   alt="" src=""/> 
+            </div>
+            <div className="Work-example__text-part">
+                <a href="#" className="Work-example__click" target='blank'>
+                    <h1 className="Work-example__title">Example 6</h1>
+                </a>
+            </div> 
+        </div>
+        <div className="Work-example e7">
+            <div className="Work-example-img-wrapper">
+                <img className="Work-example-img"   alt="" src=""/> 
+            </div>
+            <div className="Work-example__text-part">
+                <a href="#" className="Work-example__click" target='blank'>
+                    <h1 className="Work-example__title">Example 7</h1>
+                </a>
+            </div> 
+        </div>
+        <div className="Work-example e8">
+            <div className="Work-example-img-wrapper">
+                <img className="Work-example-img"   alt="" src=""/> 
+            </div>
+            <div className="Work-example__text-part">
+                <a href="#" className="Work-example__click" target='blank'>
+                    <h1 className="Work-example__title">Example 8</h1>
+                </a>
+            </div> 
+        </div>
+        
+
+
+
+
     </div>
   );
 }
